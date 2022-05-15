869ef0a4a09b336ea9b1055cc1ba9b4c9d848b39
DK\wcw <wcw@sitecore.net>
2022-05-15 14:06:29 +0800

    add navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <AppMenu />
+  <app-header />
   <router-view />
 </template>
+
+<script lang="ts">
+import AppMenu from "@/components/AppMenu.vue";
+export default {
+  components: {
+    AppMenu,
+  },
+};
+</script>
 
 <style lang="less">
 #app {
---
+++ src/components/HomeContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeContent",
+  props: {
+    msg: String,
+  },
+});
+</script>
